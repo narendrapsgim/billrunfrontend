2d62de34804d2d6aebc9d838bdea6ed7a2ccc3b0
Roman <roman@billrun.net>
2016-04-25 12:38:44 +0300

    List : Display error messagesd + Paginatin (not finished)

--- public/js/components/List/List.js
+++ public/js/components/List/List.js
@@ -9,14 +9,21 @@ import TableFooter from 'material-ui/lib/table/table-footer';
 import TextField from 'material-ui/lib/text-field';
 import Toggle from 'material-ui/lib/toggle';
 import FloatingActionButton from 'material-ui/lib/floating-action-button';
-import ContentAdd from 'material-ui/lib/svg-icons/content/add';
+import IconBack from 'material-ui/lib/svg-icons/navigation/arrow-back';
+import IconForward from 'material-ui/lib/svg-icons/navigation/arrow-forward';
 import Divider from 'material-ui/lib/divider';
+import Snackbar from 'material-ui/lib/snackbar';
+import IconButton from 'material-ui/lib/icon-button';
+import RaisedButton from 'material-ui/lib/raised-button';
 import _ from 'lodash';
 import aja from 'aja';
 
 const styles = {
-  plusButton : {
+  paginationButton : {
     margin : '10px',
+  },
+  paginationArrow : {
+    margin : '5px',
   },
   table : {
     tableLayout : 'auto',
@@ -35,9 +42,11 @@ export default class List extends React.Component {
 
   constructor(props) {
     super(props);
-    this.buttonClick = this.buttonClick.bind(this);
+    this.paginationButton = this.paginationButton.bind(this);
+    this.updateTableData = this.updateTableData.bind(this);
     this.getData = _.debounce(this.getData.bind(this),1000);
     this.filterData = this.filterData.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
 
     this.state = {
       fixedHeader : true,
@@ -48,29 +57,74 @@ export default class List extends React.Component {
       multiSelectable : true,
       enableSelectAll : true,
       deselectOnClickaway : false,
-      height : '300px',
-      rows : []
+      height : props.settings.defaults.tableHeight || '300px',
+      rows : [],
+      filters : {},
+      snackbarOpen : false,
+      snackbarMessage : '',
+      currentPage : 1,
+      totalPages : 1
     };
   }
 
-  buttonClick(e) {
-    console.log(e);
+  updateTableData(){
+    this.getData(this.buildSearchQuery());
+  }
+
+  paginationButton(e) {
+    console.log("e.currentTarget.value : ", e.currentTarget.value);
+    let page = this.state.currentPage;
+    let value = e.currentTarget.value;
+
+    if(_.isInteger(parseInt(value))){
+      page = parseInt(value);
+    } else if(value == 'back' && page > 0){
+      page --;
+    } else if(value == 'forward' && page < 9){
+      page++;
+    }
+
+    this.setState({
+      currentPage: page,
+    }, this.updateTableData);
   }
 
   filterData(e, value) {
-    this.getData(this.buildSearchQuery(value, e.target.name));
+    let filters = Object.assign({}, this.state.filters);
+    filters[e.target.name] = value;
+    this.setState({
+      filters : filters,
+    }, this.updateTableData);
+
   }
 
-  buildSearchQuery(value, key){
-    let query = '';
-    let filters = {};
-    
-    filters[key] = {
-      "$regex" : value,
-      "$options" : "i"
-    };
+  buildSearchQuery(){
 
-    query = '?query=' +  JSON.stringify(filters);
+    //5000000429,5000000986
+    let query = '?';
+    let filters = {};
+
+    for ( const key in this.state.filters ) {
+      if(this.state.filters[key].length){
+        query += key + "=" + this.state.filters[key];
+      }
+    }
+
+    // switch (key) {
+    //   case 'name':
+    //   filters[key] = {
+    //     "$regex" : value,
+    //     "$options" : "i"
+    //   };
+    //   query = 'query=' +  JSON.stringify(filters);
+    //     break;
+    //   default: query = key + '=' + value;
+    // }
+
+    if(_.isObject(this.props.settings.pagination) && _.isInteger(this.props.settings.pagination.itemsPerPage ) && this.props.settings.pagination.itemsPerPage > -1){
+      query += '&size=' + this.props.settings.pagination.itemsPerPage + '&page=' + parseInt(this.state.currentPage);
+    }
+
     return query;
   }
 
@@ -84,16 +138,28 @@ export default class List extends React.Component {
        .method('get')
        .url(url)
        .on('200',
-           (response) => {
+         (response) => {
+           if(response && response.status){
              this.setState({
                rows : response.details.slice(),
+               totalPages : 10
              });
+           } else {
+             this.handleError(response);
+           }
            })
        .go();
   }
 
   componentDidMount() {
       this.getData()
+  }
+
+  handleError(data){
+    this.setState({
+      snackbarOpen: true,
+      snackbarMessage: data.desc,
+    });
   }
 
   formatField(row, field){
@@ -111,12 +177,25 @@ export default class List extends React.Component {
     return output;
   }
 
+  handleRequestClose(){
+    this.setState({
+      snackbarOpen: false,
+      snackbarMessage: '',
+    });
+  };
+
   render() {
 
     let filters = (
         this.props.settings.fields.map((field, i) => {
           if(field.filter && field.filter == true){
-            return <TextField style={styles.filterInput} ref={"listFilter_"+ i} key={i} name={field.key} hintText={"enter " + field.label + "..."} floatingLabelText={"Search by " + field.label} onChange={this.filterData} />
+            return <TextField
+              style={styles.filterInput}
+              ref={"listFilter_"+ i}
+              key={i} name={field.key}
+              hintText={"enter " + field.label + "..."}
+              floatingLabelText={"Search by " + field.label}
+              onChange={this.filterData} />
           }
         })
     );
@@ -139,14 +218,48 @@ export default class List extends React.Component {
               </TableRow>
     ));
 
+    let pagination = '';
+    if (this.state.totalPages > 1){
+      let pagination = (
+        <TableRowColumn colSpan="3" style={{textAlign: 'center'}}>
+          <FloatingActionButton
+            value='back'
+            mini={false}
+            key={0}
+            style={styles.paginationArrow}
+            onMouseUp={this.paginationButton}>
+             <IconBack />
+          </FloatingActionButton>
+
+          {[...Array(this.setState.totalPages)].map((x, i) =>
+            <FloatingActionButton
+              onClick={this.paginationButton}
+              secondary={this.state.currentPage == (i+1)}
+              value={i+1}
+              mini={true}
+              key={i}
+              style={styles.paginationButton}>
+              {i+1}
+            </FloatingActionButton>
+          )}
+
+          <FloatingActionButton
+            value='forward'
+            mini={false}
+            key={11}
+            style={styles.paginationArrow}
+            onMouseUp={this.paginationButton}>
+             <IconForward />
+          </FloatingActionButton>
+        </TableRowColumn>
+    );
+  }
+
+
     let footer = (
                 <TableFooter>
                   <TableRow>
-                    <TableRowColumn colSpan="3" style={{textAlign: 'center'}}>
-                      <FloatingActionButton style={styles.plusButton} onMouseUp={this.buttonClick}>
-                        <ContentAdd />
-                      </FloatingActionButton>
-                    </TableRowColumn>
+                    {pagination}
                   </TableRow>
                 </TableFooter>
     );
@@ -177,6 +290,14 @@ export default class List extends React.Component {
         </TableBody>
           {footer}
         </Table>
+
+        <Snackbar
+          open={this.state.snackbarOpen}
+          message={this.state.snackbarMessage}
+          autoHideDuration={4000}
+          onRequestClose={this.handleRequestClose}
+        />
+
       </div>
     );
   }
