6b9d357c3e8bbc7471e95a14f1bf5dc4d70ef45a
Roman <roman@billrun.net>
2016-05-09 13:13:35 +0300

    Top Bar : changed to material-ui component

--- public/js/components/Topbar/index.js
+++ public/js/components/Topbar/index.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import Avatar from 'material-ui/Avatar';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import {indigo50} from 'material-ui/styles/colors';
+
 
 export default class Topbar extends Component {
   constructor(props) {
@@ -8,25 +15,32 @@ export default class Topbar extends Component {
 
   render() {
     return (
-      <div className="navbar navbar-inverse">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="#">
-              {<img src="img/billrun-logo-tm.png" />}
-            </a>
-          </div>
-          <div className="collapse navbar-collapse">
-            <div className="navbar-header navbar-right">
-                <a className="navbar-brand" href="#">
-                  <Avatar
-                    src="https://avatars.githubusercontent.com/u/1040582?v=3"
-                    size={50}
-                  />User Name
-                </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Toolbar className="topbar">
+        <ToolbarGroup firstChild={true}>
+          <a className="navbar-brand" href="#">
+            {<img src="img/billrun-logo-tm.png" />}
+          </a>
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <ToolbarSeparator style={{backgroundColor:indigo50}}/>
+          <Avatar
+            src="https://avatars.githubusercontent.com/u/1040582?v=3"
+            size={50}
+            style={{margin:'0 10px'}}
+          />
+        <ToolbarTitle style={{color:indigo50, paddingRight: 0}} text="Ofer Cohen" />
+          <IconMenu style={{marginTop:'5px'}}
+            iconButtonElement={
+              <IconButton touch={true}>
+                <NavigationExpandMoreIcon color={indigo50} />
+              </IconButton>
+            }
+          >
+            <MenuItem primaryText="Profile" />
+            <MenuItem primaryText="Settings" />
+          </IconMenu>
+        </ToolbarGroup>
+      </Toolbar>
     );
   }
 }
