ceda4bfb34080cfbc743e7fc25a93eb254e570cb
Spyes <lewis@billrun.net>
2016-08-17 09:58:58 +0300

    new subscription with return url

--- public/js/components/Subscriber/SubscriberEdit.js
+++ public/js/components/Subscriber/SubscriberEdit.js
@@ -100,7 +100,7 @@ class SubscriberEdit extends Component {
   }
 
   onClickNewSubscription(aid) {
-    window.location = `${globalSetting.serverUrl}/internalpaypage?aid=${aid}&return_url=${globalSetting.serverUrl}/subscriber?action=update&aid=${aid}`;
+    window.location = `${globalSetting.serverUrl}/internalpaypage?aid=${aid}&return_url="${globalSetting.serverUrl}/subscriber?action=update&aid=${aid}"`;
   }
   
   render() {
