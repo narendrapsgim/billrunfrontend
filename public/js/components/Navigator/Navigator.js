d3ed23ae3b74826a5950bc0e61fdc022ffb59c3a
shai <shai.shwartz.billrun.com>
2016-08-31 15:02:24 +0300

    changes in the navigator

--- public/js/components/Navigator/Navigator.js
+++ public/js/components/Navigator/Navigator.js
@@ -1,209 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import classNames from "classnames";
 
 export default class Navigator extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      uiOpenSetting: false
+    };
   }
 
+  toggleMenu(){
+    if($(".navbar-collapse").hasClass('collapse')){
+      $(".navbar-collapse").removeClass('collapse');  
+    }
+    else{
+      $(".navbar-collapse").addClass('collapse');
+    }
+  }
+  
   render() {
     return (
       <nav className="navbar navbar-default navbar-static-top" role="navigation" style={{"marginBottom": "0px"}}>
         <div className="navbar-header">
-          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <button type="button" className="navbar-toggle" onClick={this.toggleMenu} style={{position: 'absolute', right: 0, top: 0}}>
             <span className="sr-only">Toggle navigation</span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand" href="index.html">SB Admin v2.0</a>
+          <a className="navbar-brand" href="index.html">Billrun</a>
         </div>
 
         <ul className="nav navbar-top-links navbar-right">
-          <li className="dropdown">
-            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-              <i className="fa fa-envelope fa-fw"></i> <i className="fa fa-caret-down"></i>
-            </a>
-            <ul className="dropdown-menu dropdown-messages">
-              <li>
-                <a href="#">
-                  <div>
-                    <strong>John Smith</strong>
-                    <span className="pull-right text-muted">
-                      <em>Yesterday</em>
-                    </span>
-                  </div>
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <strong>John Smith</strong>
-                    <span className="pull-right text-muted">
-                      <em>Yesterday</em>
-                    </span>
-                  </div>
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <strong>John Smith</strong>
-                    <span className="pull-right text-muted">
-                      <em>Yesterday</em>
-                    </span>
-                  </div>
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a className="text-center" href="#">
-                  <strong>Read All Messages</strong>
-                  <i className="fa fa-angle-right"></i>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className="dropdown">
-            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-              <i className="fa fa-tasks fa-fw"></i> <i className="fa fa-caret-down"></i>
-            </a>
-            <ul className="dropdown-menu dropdown-tasks">
-              <li>
-                <a href="#">
-                  <div>
-                    <p>
-                      <strong>Task 1</strong>
-                      <span className="pull-right text-muted">40% Complete</span>
-                    </p>
-                    <div className="progress progress-striped active">
-                      <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style={{"width": "40%"}}>
-                        <span className="sr-only">40% Complete (success)</span>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <p>
-                      <strong>Task 2</strong>
-                      <span className="pull-right text-muted">20% Complete</span>
-                    </p>
-                    <div className="progress progress-striped active">
-                      <div className="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style={{"width": "20%"}}>
-                        <span className="sr-only">20% Complete</span>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <p>
-                      <strong>Task 3</strong>
-                      <span className="pull-right text-muted">60% Complete</span>
-                    </p>
-                    <div className="progress progress-striped active">
-                      <div className="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{"width": "60%"}}>
-                        <span className="sr-only">60% Complete (warning)</span>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <p>
-                      <strong>Task 4</strong>
-                      <span className="pull-right text-muted">80% Complete</span>
-                    </p>
-                    <div className="progress progress-striped active">
-                      <div className="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style={{"width": "80%"}}>
-                        <span className="sr-only">80% Complete (danger)</span>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a className="text-center" href="#">
-                  <strong>See All Tasks</strong>
-                  <i className="fa fa-angle-right"></i>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className="dropdown">
-            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-              <i className="fa fa-bell fa-fw"></i> <i className="fa fa-caret-down"></i>
-            </a>
-            <ul className="dropdown-menu dropdown-alerts">
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-comment fa-fw"></i> New Comment
-                    <span className="pull-right text-muted small">4 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-twitter fa-fw"></i> 3 New Followers
-                    <span className="pull-right text-muted small">12 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-envelope fa-fw"></i> Message Sent
-                    <span className="pull-right text-muted small">4 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-tasks fa-fw"></i> New Task
-                    <span className="pull-right text-muted small">4 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-upload fa-fw"></i> Server Rebooted
-                    <span className="pull-right text-muted small">4 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a className="text-center" href="#">
-                  <strong>See All Alerts</strong>
-                  <i className="fa fa-angle-right"></i>
-                </a>
-              </li>
-            </ul>
-          </li>
           <li className="dropdown">
             <a className="dropdown-toggle" data-toggle="dropdown" href="#">
               <i className="fa fa-user fa-fw"></i> <i className="fa fa-caret-down"></i>
@@ -239,81 +68,34 @@ export default class Navigator extends Component {
                 </Link>
               </li>
               <li>
-                <a href="#"><i className="fa fa-bar-chart-o fa-fw"></i> Charts<span className="fa arrow"></span></a>
+                <a href="#"><i className="fa fa-bar-chart-o fa-fw"></i> Products</a>
                 <ul className="nav nav-second-level">
-                  <li>
-                    <a href="flot.html">Flot Charts</a>
-                  </li>
-                  <li>
-                    <a href="morris.html">Morris.js Charts</a>
-                  </li>
                 </ul>
               </li>
               <li>
-                <a href="tables.html"><i className="fa fa-table fa-fw"></i> Tables</a>
+                <a href="customers.html"><i className="fa fa-table fa-fw"></i> Customers</a>
               </li>
               <li>
-                <a href="forms.html"><i className="fa fa-edit fa-fw"></i> Forms</a>
+                <a href="usage.html"><i className="fa fa-edit fa-fw"></i> Usage</a>
               </li>
               <li>
-                <a href="#"><i className="fa fa-wrench fa-fw"></i> UI Elements<span className="fa arrow"></span></a>
-                <ul className="nav nav-second-level">
-                  <li>
-                    <a href="panels-wells.html">Panels and Wells</a>
-                  </li>
-                  <li>
-                    <a href="buttons.html">Buttons</a>
-                  </li>
-                  <li>
-                    <a href="notifications.html">Notifications</a>
-                  </li>
-                  <li>
-                    <a href="typography.html">Typography</a>
-                  </li>
-                  <li>
-                    <a href="icons.html"> Icons</a>
-                  </li>
-                  <li>
-                    <a href="grid.html">Grid</a>
-                  </li>
-                </ul>
+                <a href="#"><i className="fa fa-wrench fa-fw"></i> Invoices</a>
               </li>
               <li>
-                <a href="#"><i className="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span className="fa arrow"></span></a>
-                <ul className="nav nav-second-level">
-                  <li>
-                    <a href="#">Second Level Item</a>
-                  </li>
-                  <li>
-                    <a href="#">Second Level Item</a>
-                  </li>
-                  <li>
-                    <a href="#">Third Level <span className="fa arrow"></span></a>
-                    <ul className="nav nav-third-level">
-                      <li>
-                        <a href="#">Third Level Item</a>
-                      </li>
-                      <li>
-                        <a href="#">Third Level Item</a>
-                      </li>
-                      <li>
-                        <a href="#">Third Level Item</a>
-                      </li>
-                      <li>
-                        <a href="#">Third Level Item</a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
+                <a href="#"><i className="fa fa-sitemap fa-fw"></i> Log</a>
+                
               </li>
-              <li>
-                <a href="#"><i className="fa fa-files-o fa-fw"></i> Sample Pages<span className="fa arrow"></span></a>
-                <ul className="nav nav-second-level">
+              <li className={classNames({'active': !this.state.uiOpenSetting})}>
+                <a href="javascript:void(0)" onClick={ ()=> this.setState({ uiOpenSetting: !this.state.uiOpenSetting })}><i className="fa fa-files-o fa-fw"></i> Setting<span className="fa arrow"></span></a>
+                <ul className={classNames({'nav nav-second-level': true, 'collapse': this.state.uiOpenSetting})}>
                   <li>
-                    <a href="blank.html">Blank Page</a>
+                    <a href="blank.html">Date, Time and Zone</a>
                   </li>
                   <li>
-                    <a href="login.html">Login Page</a>
+                    <a href="login.html">Currency and Tax</a>
+                  </li>
+                  <li>
+                    <a href="login.html">Input Processor</a>
                   </li>
                 </ul>
               </li>
