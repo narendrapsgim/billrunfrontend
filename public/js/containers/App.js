8482f450d3e12f0094f3c511f8c0a5ed88be06a5
Roman <roman@billrun.net>
2016-08-31 15:28:07 +0300

    Layout

--- public/js/containers/App.js
+++ public/js/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Panel, PageHeader, Col, Row} from 'react-bootstrap';
 
 import Navigator from '../components/Navigator';
 
@@ -6,18 +7,36 @@ import Navigator from '../components/Navigator';
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      title: props.routes[props.routes.length-1].title || props.routes[props.routes.length-1].name
+    }
   }
 
   componentWillMount() {
     this.setState({Height: "100%"});
   }
-  
+
+  componentWillReceiveProps(nextProps) {
+    console.log("componentWillReceiveProps");
+    this.setState({
+      title: nextProps.routes[nextProps.routes.length-1].title || nextProps.routes[nextProps.routes.length-1].name
+    });
+  }
+
   render() {
     return (
-      <div>
+      <div id="wrapper">
         <Navigator />
-        <div id="page-wrapper" className="page-wrapper" ref="pageWrapper" style={{minHeight: this.state.Height}}>
-          { this.props.children }
+        <div id="page-wrapper" style={{minHeight: this.state.Height}}>
+          <Row>
+            <Col lg={12}>
+                <PageHeader>{this.state.title}</PageHeader>
+            </Col>
+          </Row>
+          <Row>
+            { this.props.children }
+          </Row>
         </div>
       </div>
     );
