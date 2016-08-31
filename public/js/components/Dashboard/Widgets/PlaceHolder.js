6f0657f63c85d4da64f314c3215e28d526b386d2
Roman <roman@billrun.net>
2016-08-31 15:38:21 +0300

    Dashboard - fix for loading placeholder

--- public/js/components/Dashboard/Widgets/PlaceHolder.js
+++ public/js/components/Dashboard/Widgets/PlaceHolder.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 export default class PlaceHolder extends Component {
 
   getStyles() {
-    return  { width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'};
+    return  { width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'};
   }
 
   render() {
