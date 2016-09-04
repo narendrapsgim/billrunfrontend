242a3aab0a313c16ab952255de8535895c8afd2b
Roman <roman@billrun.net>
2016-09-04 20:57:31 +0300

    .gitignore updated

--- public/js/components/Charts/MapChart.js
+++ public/js/components/Charts/MapChart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Datamap from 'react-datamaps';
 
-
+// https://github.com/markmarkoh/datamaps/blob/master/README.md#getting-started
 export default class MapChart extends React.Component {
   constructor(props) {
     super(props);
