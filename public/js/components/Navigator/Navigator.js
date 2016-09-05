80eb6d09758ab0605cc32ec45f9eba4239c1eca0
Roman <roman@billrun.net>
2016-09-05 19:12:19 +0300

    Menu with fixd rout + logout

--- public/js/components/Navigator/Navigator.js
+++ public/js/components/Navigator/Navigator.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Link } from 'react-router';
-import { userDoLogout } from '../../actions/userActions';
 import classNames from "classnames";
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem, ProgressBar} from "react-bootstrap";
+
+import { userDoLogout } from '../../actions/userActions';
+
 
 class Navigator extends Component {
   constructor(props) {
@@ -23,25 +25,29 @@ class Navigator extends Component {
     return (
       <nav className="navbar navbar-default navbar-static-top" role="navigation" style={{"marginBottom": "0px"}}>
         <div className="navbar-header">
-          <a className="navbar-brand" href="index.html">Billrun</a>
+          <Link to="/" className="navbar-brand">
+            BillRun!
+          </Link>
         </div>
 
         <ul className="nav navbar-top-links navbar-right">
           <NavDropdown  title={<i className="fa fa-user fa-fw"></i>} >
+          {/*
             <MenuItem eventKey="1">
               <i className="fa fa-user fa-fw"></i> User Profile
-            </MenuItem> 
+            </MenuItem>
             <MenuItem eventKey="2">
               <i className="fa fa-gear fa-fw"></i> Settings
-            </MenuItem> 
+            </MenuItem>
             <MenuItem eventKey="3">
               <a href="http://www.strapui.com/" onClick={ () => { window.location='http://www.strapui.com/'; } }>
                 <i className="fa fa-eye fa-fw"></i> Premium React Themes
               </a>
-            </MenuItem> 
+            </MenuItem>
             <MenuItem divider />
+          */}
             <MenuItem eventKey="4">
-              <Link to="login">
+              <Link to="#" onClick={this.clickLogout}>
                 <i className="fa fa-sign-out fa-fw"></i> Logout
               </Link>
             </MenuItem>
@@ -53,19 +59,19 @@ class Navigator extends Component {
 
             <ul className="nav in" id="side-menu">
               <li>
-                <Link to="/Dashboard">
+                <Link to="/dashboard">
                   <i className="fa fa-dashboard fa-fw"></i> Dashboard
                 </Link>
               </li>
 
               <li>
                 <Link to="/plans">
-                  <i className="fa fa-dashboard fa-fw"></i> Plans
+                  <i className="fa fa-table fa-fw"></i> Plans
                 </Link>
               </li>
               <li>
                 <Link to="/products">
-                  <i className="fa fa-bar-chart-o fa-fw"></i> Products
+                  <i className="fa fa-table fa-fw"></i> Products
                 </Link>
               </li>
               <li>
@@ -78,16 +84,18 @@ class Navigator extends Component {
                   <Link to="#"><i className="fa fa-wrench fa-fw"></i> Invoices</Link>
                   </li> */}
               {/* <li>
-                  <Link to="#"><i className="fa fa-sitemap fa-fw"></i> Log</Link>                
+                  <Link to="#"><i className="fa fa-sitemap fa-fw"></i> Log</Link>
                   </li> */}
               <li className={classNames({'active': !this.state.uiOpenSetting})}>
-                <Link to="javascript:void(0)" onClick={ ()=> this.setState({ uiOpenSetting: !this.state.uiOpenSetting })}><i className="fa fa-files-o fa-fw"></i> Setting<span className="fa arrow"></span></Link>
+                <Link to="javascript:void(0)" onClick={ ()=> this.setState({ uiOpenSetting: !this.state.uiOpenSetting })}>
+                  <i className="fa fa-gears fa-fw"></i> Setting<span className="fa arrow"></span>
+                </Link>
                 <ul className={classNames({'nav nav-second-level': true, 'collapse': this.state.uiOpenSetting})}>
                   <li>
                     <Link to="/settings?setting=billrun">Date, Time and Zone</Link>
                   </li>
                   <li>
-                    <Link to="settings?setting=pricing">Currency and Tax</Link>
+                    <Link to="/settings?setting=pricing">Currency and Tax</Link>
                   </li>
                   <li>
                     <Link to="/input_processors">Input Processors</Link>
--- public/js/containers/App.js
+++ public/js/containers/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      title: props.routes[props.routes.length-1].title || props.routes[props.routes.length-1].name
+      title: props.routes[props.routes.length-1].title || ''
     }
   }
 
@@ -29,7 +29,7 @@ class App extends Component {
 
   componentWillReceiveProps(nextProps) {
     this.setState({
-      title: nextProps.routes[nextProps.routes.length-1].title || nextProps.routes[nextProps.routes.length-1].name
+      title: nextProps.routes[nextProps.routes.length-1].title || ''
     });
   }
 
@@ -51,7 +51,7 @@ class App extends Component {
         <div id="page-wrapper" className="page-wrapper" ref="pageWrapper" style={{minHeight: this.state.Height}}>
           <Row>
             <Col lg={12}>
-              <PageHeader>{this.state.title}</PageHeader>
+              {this.state.title.length ? <PageHeader>{this.state.title}</PageHeader> : null }
             </Col>
           </Row>
           <Row>
