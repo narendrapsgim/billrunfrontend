cb082b89c03af26b9ae8c1bbd8fa1a31cec353ce
Roman <roman@billrun.net>
2016-09-06 11:24:08 +0300

    ProgressIndicator - option to switch ProgressIndicator to CSS3 animation

--- public/js/components/ProgressIndicator/ProgressIndicator.js
+++ public/js/components/ProgressIndicator/ProgressIndicator.js
@@ -11,6 +11,11 @@ class ProgressIndicator extends Component {
 
     if(!progressIndicator) return null;
 
+    // return (
+    //   <div style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1001}}>
+    //     <div className="system-progress-indecator"></div>
+    //   </div>
+    // );
     return (
       <div style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1001}}>
         <ProgressBar active now={100} style={{ height: 5, marginBottom: 0, backgroundColor: 'transparent', borderRadius: 0}}  />
