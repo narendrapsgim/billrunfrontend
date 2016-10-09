1fb017be367a743efb8e0506e8fbf02b03b77a5f
Roman <roman@billrun.net>
2016-10-09 15:08:10 +0300

    Plan Edit - refactoring sub components

---
+++ public/js/components/Plan/components/Product.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Chip from 'material-ui/Chip';
+
+export default class Product extends Component {
+
+  static propTypes = {
+    onRemoveProduct: React.PropTypes.func.isRequired,
+    product: React.PropTypes.string.isRequired
+  }
+
+  onRemoveProduct =() => {
+    const { product } = this.props;
+    this.props.onRemoveProduct(product);
+  }
+
+  render() {
+    const { product } = this.props;
+    return (
+      <Chip style={{ margin: 4 }} onRequestDelete={this.onRemoveProduct}>
+        {product}
+      </Chip>
+    );
+  }
+
+}
