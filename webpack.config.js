cfcfceaf9dceefbec54050ac7bee4bfc2bf824dc
Roman <roman@billrun.net>
2016-10-12 17:17:58 +0300

    webpack - split compiled app.js to bundle.app.js and bundle.vendor.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,7 @@ var fontAwesomePath = __dirname + '/node_modules/font-awesome/css';
 var env = process.env.NODE_ENV || 'dev';
 
 /**
- * This is the Webpack configuration file for local development. It contains
- * local-specific configuration such as the React Hot Loader, as well as:
- *
- * - The entry point of the application
- * - Where the output file should be
- * - Which loaders to use on what files to properly transpile the source
- *
+ * This is the Webpack configuration file
  * For more information, see: http://webpack.github.io/docs/configuration.html
  */
 module.exports = {
@@ -24,21 +18,19 @@ module.exports = {
   //devtool: "source-map",
 
   // Set entry point to ./src/main and include necessary files for hot load
-  entry:  [
-//    "webpack-dev-server/client?http://localhost:9090",
-    "webpack/hot/only-dev-server",
-    "./public/js/index"
-  ],
+  entry:  {
+    app : './public/js/index.js',
+    vendor: ['axios', 'material-ui', 'moment', 'lodash']
+  },
 
   // This will not actually create a bundle.js file in ./build. It is used
   // by the dev server for dynamic hot loading.
   output: {
-    path: __dirname + "/public/build/",
-    filename: "./app.js",
-//    publicPath: "http://localhost:9090/build/"
+    path: __dirname + '/public/build/',
+    filename: './bundle.[name].js'
   },
 
-  // Necessary plugins for hot load
+  // Necessary plugins
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
@@ -47,24 +39,22 @@ module.exports = {
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+    }),
     new ExtractTextPlugin('style.css', { allChunks: true }),
   ],
 
   // Transform source code using Babel and React Hot Loader
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react', 'stage-2'],
-        }
-      },
-      { test: /\.css$/, loader: "style!css" },
+      { test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/, },
+      { test: /\.css$/, loader: 'style!css' },
       { test: /\.less$/, loader: 'style!css!less' },
       { test: /\.scss$/, loader: 'style!css!sass' },
       { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
-      { test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loaders: ["file-loader"] },
-      { test: /\.json$/, loaders: ["json-loader"], include: /\.json$/}
+      { test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loaders: ['file-loader'] },
+      { test: /\.json$/, loaders: ['json-loader'], include: /\.json$/}
     ]
   },
 
@@ -80,4 +70,4 @@ module.exports = {
 //    require('postcss-nested') // Enable nested rules, like in Sass
   ]
 
-}
+};
