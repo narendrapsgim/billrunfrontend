a983ebb1478501954bcb08321f9b2985c508517e
Roman <roman@billrun.net>
2016-11-07 20:35:37 +0200

    AirBNB code Standart

--- public/js/components/ProductsList/ProductsList.js
+++ public/js/components/ProductsList/ProductsList.js
@@ -1,106 +1,103 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router';
+import Immutable from 'immutable';
 import moment from 'moment';
-
-import { PageHeader } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import List from '../List';
 import Pager from '../Pager';
 import Filter from '../Filter';
-import { Button } from "react-bootstrap";
-
 /* ACTIONS */
-import { getList } from '../../actions/listActions';
-import List from '../List';
+import { getList, clearList } from '../../actions/listActions';
 
 
 class ProductsList extends Component {
+
+  static defaultProps = {
+    items: Immutable.List(),
+  }
+
+  static propTypes = {
+    items: React.PropTypes.instanceOf(Immutable.List),
+    router: React.PropTypes.shape({
+      push: React.PropTypes.func.isRequired,
+    }).isRequired,
+    clearList: React.PropTypes.func.isRequired,
+    getList: React.PropTypes.func.isRequired,
+  }
+
   constructor(props) {
     super(props);
-
-    this.buildQuery = this.buildQuery.bind(this);
-    this.handlePageClick = this.handlePageClick.bind(this);
-    this.onNewProduct = this.onNewProduct.bind(this);
-    this.onClickProduct = this.onClickProduct.bind(this);
-
-    this.onSort = this.onSort.bind(this);
-    this.onFilter = this.onFilter.bind(this);
-
+    this.itemsType = 'products';
+    this.itemType = 'product';
     this.state = {
       page: 0,
       size: 10,
       sort: '',
-      filter: {}
+      filter: {},
     };
   }
 
-  buildQuery() {
-    return {
-      api: "find",
-      params: [
-        { collection: "rates" },
-        { size: this.state.size },
-        { page: this.state.page },
-	{ sort: this.state.sort },
-        { query: this.state.filter }
-      ]
-    };
-  }
-  
-  onClickProduct(product) {
-    this.context.router.push({
-      pathname: 'product',
-      query: {
-        action: "update",
-        productId: product.getIn(['_id', '$id'])
-      }
-    });
+  componentWillUnmount() {
+    this.props.clearList(this.itemsType);
   }
 
-  handlePageClick(page) {
-    this.setState({page}, () => {
-      this.props.dispatch(getList('products', this.buildQuery()))
-    });
+  onClickNew = () => {
+    this.props.router.push(`/${this.itemType}`);
   }
 
-  onNewProduct() {
-    this.context.router.push({
-      pathname: 'product',
-      query: {
-        action: 'new'
-      }
-    });
+  onClickItem = (item) => {
+    const itemId = item.getIn(['_id', '$id']);
+    this.props.router.push(`/${this.itemType}/${itemId}`);
   }
 
-  onFilter(filter) {
-    this.setState({filter, page: 0}, () => {
-      this.props.dispatch(getList('products', this.buildQuery()))
-    });
+  onFilter = (filter) => {
+    this.setState({ filter, page: 0 }, this.fetchItems);
   }
 
-  onSort(sort) {
-    this.setState({sort}, () => {
-      this.props.dispatch(getList('products', this.buildQuery()))
-    });
+  onSort = (sort) => {
+    this.setState({ sort }, this.fetchItems);
+  }
+
+  buildQuery = () => ({
+    api: 'find',
+    params: [
+      { collection: 'rates' },
+      { size: this.state.size },
+      { page: this.state.page },
+      { sort: this.state.sort },
+      { query: this.state.filter },
+    ],
+  });
+
+  fetchItems = () => {
+    this.props.getList(this.itemsType, this.buildQuery());
+  }
+
+  handlePageClick = (page) => {
+    this.setState({ page }, this.fetchItems);
+  }
+
+  usegtParser = (item) => {
+    const usegt = item.get('rates', Immutable.Map()).keySeq().first();
+    return (typeof usegt !== 'undefined') ? usegt : '';
   }
 
   render() {
-    const { products } = this.props;
-
-    const unit_type_by_parser = (product) => {
-      return Object.keys(product.get('rates').toJS())[0];
-    };
-
+    const { items } = this.props;
+    const baseFilter = { to: { $gt: moment().toISOString() } };
     const fields = [
-      {id: "key", placeholder: "Name"},
-      {id: "to", showFilter: false, type: "datetime"}
+      { id: 'key', placeholder: 'Name' },
+      { id: 'to', showFilter: false, type: 'datetime' },
     ];
-
     const tableFields = [
-      {id: 'key', title: 'Name', sort: true},
-      {id: 'unit_type', title: 'Unit Type', parser: unit_type_by_parser},
-      {id: 'code', title: "Code", sort: true},
-      {id: 'description', title: "Description", sort: true},
-      {id: 'from', title: 'From', type: "datetime", cssClass: 'long-date', sort: true},
-      {id: 'to', title: 'To', type: "datetime", cssClass: 'long-date', sort: true}
+      { id: 'key', title: 'Name', sort: true },
+      { id: 'unit_type', title: 'Unit Type', parser: this.usegtParser },
+      { id: 'code', title: 'Code', sort: true },
+      { id: 'description', title: 'Description', sort: true },
+      { id: 'from', title: 'From', type: 'datetime', cssClass: 'long-date', sort: true },
+      { id: 'to', title: 'To', type: 'datetime', cssClass: 'long-date', sort: true },
     ];
 
     return (
@@ -111,31 +108,30 @@ class ProductsList extends Component {
               <div className="panel-heading">
                 List of all available products
                 <div className="pull-right">
-                  <Button bsSize="xsmall" className="btn-primary" onClick={this.onNewProduct}><i className="fa fa-plus"/>&nbsp;Add New</Button>
+                  <Button bsSize="xsmall" className="btn-primary" onClick={this.onClickNew}><i className="fa fa-plus" />&nbsp;Add New</Button>
                 </div>
               </div>
               <div className="panel-body">
-                <Filter fields={fields} onFilter={this.onFilter} base={{ to: {$gt: moment().toISOString()}}} />
-                <List items={ products } fields={ tableFields } editField="key" edit={ true } onClickEdit={ this.onClickProduct } onSort={ this.onSort } />
+                <Filter fields={fields} onFilter={this.onFilter} base={baseFilter} />
+                <List items={items} fields={tableFields} editField="key" edit={true} onClickEdit={this.onClickItem} onSort={this.onSort} />
               </div>
             </div>
           </div>
         </div>
 
-        <Pager onClick={this.handlePageClick}
-               size={this.state.size}
-               count={products.size || 0} />
+        <Pager onClick={this.handlePageClick} size={this.state.size} count={items.size} />
       </div>
     );
   }
 }
 
-ProductsList.contextTypes = {
-  router: React.PropTypes.object.isRequired
-};
+const mapDispatchToProps = dispatch => bindActionCreators({
+  clearList,
+  getList,
+}, dispatch);
 
-function mapStateToProps(state, props) {
-  return { products: state.list.get('products') || [] };
-};
+const mapStateToProps = state => ({
+  items: state.list.get('products'),
+});
 
-export default connect(mapStateToProps)(ProductsList);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ProductsList));
