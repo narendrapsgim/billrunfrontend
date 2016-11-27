d15002915679057126633fa88f9c2586016f1b4b
Roman <roman@billrun.net>
2016-11-27 13:59:11 +0200

    BRCD-31 : other fixed/changes reqired by collections

--- public/js/reducers/guiState/index.js
+++ public/js/reducers/guiState/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import deleteConfirmShow from './deleteConfirmShowReducer';
 import page from './pageReducer';
 
 
 export default combineReducers({
-  deleteConfirmShow,
   page,
 });
