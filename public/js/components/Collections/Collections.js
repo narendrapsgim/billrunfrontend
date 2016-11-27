47e94cadd2eca19b36826da402fe8413d4d0a71b
Roman <roman@billrun.net>
2016-11-27 13:48:02 +0200

    BRCD-31 : Collections - View / Edit / Create / Delete

--- public/js/components/Collections/Collections.js
+++ public/js/components/Collections/Collections.js
@@ -1,95 +1,75 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import Immutable from 'immutable';
-import { getList, clearList } from '../../actions/listActions';
-import { toggleDeleteConfirm } from '../../actions/guiStateActions/deleteConfirmAction';
-
 import CollectionItemDisplay from './Elements/CollectionItemDisplay';
 import CollectionItemAdd from './Elements/CollectionItemAdd';
-import ConfirmDelete from '../Elements/ConfirmDelete';
-// DEV ONLY
-import collectionStub from './stub_collection.json';
+import { getSettings, saveSettings } from '../../actions/settingsActions';
+import { removeCollection } from '../../actions/collectionsActions';
+
 
 class Collections extends Component {
+
+  static defaultProps = {
+    collections: Immutable.List(),
+  };
+
   static propTypes = {
-    collections: React.PropTypes.instanceOf(Immutable.List)
-  }
-
-  constructor(props) {
-    super(props);
-
-    this.buildQuery = this.buildQuery.bind(this);
-
-    this.state = {
-      page: 0,
-      sort: 'days'
-    };
-
-    this.confirmDeleteModal = this.confirmDeleteModal.bind(this);
+    collections: PropTypes.instanceOf(Immutable.List),
+    dispatch: PropTypes.func.isRequired,
+    router: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
   }
 
   componentDidMount() {
-    // this.props.getList("collections", this.buildQuery());
+    this.props.dispatch(getSettings('collection'));
   }
 
-  buildQuery() {
-    return {
-      api: "find",
-      params: [
-        { collection: "collections" }/*,
-        { size: this.state.size },
-        { page: this.state.page },
-        { sort: this.state.sort },
-        { query: this.state.filter }*/
-      ]
-    };
+  shouldComponentUpdate(nextProps) {
+    return !Immutable.is(this.props.collections, nextProps.collections);
   }
 
-  confirmDeleteModal (collection_name = '') {
-    let msg = 'Please confirm ' + collection_name;
-    console.log('msg ', msg );
-    this.props.dispatch(toggleDeleteConfirm(msg));
+  onRemove = (id) => {
+    const { collections } = this.props;
+    const index = collections.findIndex(item => item.get('id') === id);
+    if (index > -1) {
+      this.props.dispatch(removeCollection(index));
+      this.props.dispatch(saveSettings('collection'));
+    }
   }
+
+  onClickNew = () => {
+    this.props.router.push('/collection');
+  }
+
+  onClickEdit = (id) => {
+    this.props.router.push(`/collection/${id}`);
+  }
+
+  sortCollections = (collectionA, collectionB) => collectionA.get('do_after_days') > collectionB.get('do_after_days')
+
+  renderCollection = item => (
+    <CollectionItemDisplay item={item} key={item.get('id')} onRemove={this.onRemove} onEdit={this.onClickEdit} />
+  )
 
   render() {
-    let collections = this.props.collections;
-
+    const { collections } = this.props;
     return (
       <div className="row collections-list col-lg-12 clearfix">
         <div className="panel panel-default">
           <div className="col-md-12">
-            {collections.map((entity, index) => {
-              return (
-                <CollectionItemDisplay item={entity} index={index} key={index} onRemoveClick={this.confirmDeleteModal}/>
-              )
-            })}
-
-            <CollectionItemAdd />
+            { collections.sort(this.sortCollections).map(this.renderCollection) }
+            <CollectionItemAdd onClickNew={this.onClickNew} />
           </div>
         </div>
-
-        <ConfirmDelete  />
       </div>
     );
   }
 }
 
-Collections.contextTypes = {
-  router: React.PropTypes.object.isRequired
-};
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    getList,
-    toggleDeleteConfirm
-  }, dispatch);
-}
-
-function mapStateToProps(state, props) {
-  return {
-    collections: state.list.get('collections') || Immutable.fromJS(collectionStub)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Collections);
+const mapStateToProps = state => ({
+  collections: state.settings.get('collection'),
+});
+export default withRouter(connect(mapStateToProps)(Collections));
