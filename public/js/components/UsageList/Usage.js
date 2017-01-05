d8e928f3d36599bbe827c3b07265f046993af8d3
Roman <roman@billrun.net>
2017-01-05 21:02:25 +0200

    BRCD-535 : Ussage screen - Lint + refactoring

--- public/js/components/UsageList/Usage.js
+++ public/js/components/UsageList/Usage.js
@@ -1,45 +1,56 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
+import Immutable from 'immutable';
 import { getFieldName } from '../../common/Util';
 
-export default class Usage extends Component {
-  constructor(props) {
-    super(props);
-  }
-  
-  render() {
-    const { line, onClickCancel } = this.props;
-    const hidden_fields = [
-      "_id", "in_plan", "over_plan", "interconnect_aprice", "out_plan"
-    ];
 
+const Usage = ({ line, onClickCancel, hiddenFields, cancelLabel }) => {
+  const renderFields = () => line.keySeq().map((field, key) => {
+    if (hiddenFields.includes(field)) {
+      return (null);
+    }
     return (
-      <form className="form-horizontal">
-	<div className="form-group">
-	  <div className="col-lg-12">
-	    <button type="button" onClick={onClickCancel} className="btn btn-default">Back</button>
-	  </div>
-	</div>
-	<div className="form-group">
-	  <div className="col-lg-12">
-	    <div className="panel panel-default">
-	      <div className="panel-body">
-		{line.keySeq().map((field, key) => {
-		   if (hidden_fields.includes(field)) return (null);
-		   return (
-		     <div className="form-group" key={key}>
-		       <label className="col-lg-2 control-label">{ getFieldName(field, 'lines') }</label>
-		       <div className="col-lg-4">
-			 <input disabled className="form-control" value={line.get(field)} />
-		       </div>
-		     </div>
-		   );
-		})}
-	      </div>
-	    </div>
-	    <button type="button" onClick={onClickCancel} className="btn btn-default">Back</button>
-	  </div>
-	</div>
-      </form>
+      <div className="form-group" key={key}>
+        <label className="col-lg-2 control-label">{ getFieldName(field, 'lines') }</label>
+        <div className="col-lg-4">
+          <input disabled className="form-control" value={line.get(field)} />
+        </div>
+      </div>
     );
-  }
-}
+  });
+
+  return (
+    <form className="form-horizontal">
+      <div className="form-group">
+        <div className="col-lg-12">
+          <button type="button" onClick={onClickCancel} className="btn btn-default">{cancelLabel}</button>
+        </div>
+      </div>
+      <div className="form-group">
+        <div className="col-lg-12">
+          <div className="panel panel-default">
+            <div className="panel-body">
+              { renderFields() }
+            </div>
+          </div>
+          <button type="button" onClick={onClickCancel} className="btn btn-default">{cancelLabel}</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+Usage.defaultProps = {
+  line: Immutable.Map(),
+  hiddenFields: ['_id', 'in_plan', 'over_plan', 'interconnect_aprice', 'out_plan'],
+  onClickCancel: () => {},
+  cancelLabel: 'Back',
+};
+
+Usage.propTypes = {
+  line: PropTypes.instanceOf(Immutable.Record),
+  hiddenFields: PropTypes.element,
+  onClickCancel: PropTypes.func,
+  cancelLabel: PropTypes.string,
+};
+
+export default Usage;
