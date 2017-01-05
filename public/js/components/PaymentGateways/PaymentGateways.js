8938af467fc6ede6e1c711053109746751611a4d
Roman <roman@billrun.net>
2017-01-05 21:37:25 +0200

    payment_gateways: set default PG to empty array + refactoring

--- public/js/components/PaymentGateways/PaymentGateways.js
+++ public/js/components/PaymentGateways/PaymentGateways.js
@@ -1,112 +1,107 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getSettings, addPaymentGateway, removePaymentGateway, updatePaymentGateway } from '../../actions/settingsActions';
-import { apiBillRun } from '../../common/Api';
-import { showSuccess, showDanger } from '../../actions/alertsActions';
-import { getList, getPaymentGateways } from '../../actions/listActions';
 import Immutable from 'immutable';
-
 import PaymentGateway from './PaymentGateway';
+import { apiBillRun } from '../../common/Api';
+import { savePaymentGatewayQuery, disablePaymentGatewayQuery } from '../../common/ApiQueries';
+
+import { getSettings, addPaymentGateway, removePaymentGateway, updatePaymentGateway } from '../../actions/settingsActions';
+import { showSuccess, showDanger } from '../../actions/alertsActions';
+import { getPaymentGateways } from '../../actions/listActions';
 
 class PaymentGateways extends Component {
-  constructor(props) {
-    super(props);
-  }
+
+  static defaultProps = {
+    payment_gateways: Immutable.List(),
+    supported_gateways: Immutable.List(),
+  };
+
+  static propTypes = {
+    dispatch: React.PropTypes.func.isRequired,
+    payment_gateways: React.PropTypes.instanceOf(Immutable.List),
+    supported_gateways: React.PropTypes.instanceOf(Immutable.List),
+  };
 
   componentDidMount() {
-    this.props.dispatch(getSettings("payment_gateways"));
+    this.props.dispatch(getSettings('payment_gateways'));
     this.props.dispatch(getPaymentGateways());
   }
 
   onSaveGatewayParams = (gateway, enabled) => {
-    const { dispatch } = this.props;
-    const query = {
-      api: "settings",
-      params: [
-	{ category: "payment_gateways" },
-	{ action: "set" },
-	{ data: JSON.stringify(gateway) }
-      ]
-    };
-
-    apiBillRun(query).then(
-      success => {
-	dispatch(showSuccess("Payment gateway enabled!"));
-	if (!enabled) dispatch(addPaymentGateway(gateway));
-	else dispatch(updatePaymentGateway(gateway));
+    apiBillRun(savePaymentGatewayQuery(gateway)).then(
+      (success) => { // eslint-disable-line no-unused-vars
+        this.props.dispatch(showSuccess('Payment gateway enabled!'));
+        if (!enabled) {
+          this.props.dispatch(addPaymentGateway(gateway));
+        } else {
+          this.props.dispatch(updatePaymentGateway(gateway));
+        }
       },
-      failure => {
-	dispatch(showDanger("Error saving payment gateway"));
+      (failure) => { // eslint-disable-line no-unused-vars
+        this.props.dispatch(showDanger('Error saving payment gateway'));
       }
     ).catch(
-      error => {
-	dispatch(showDanger("Network error - please try again"));
+      (error) => { // eslint-disable-line no-unused-vars
+        this.props.dispatch(showDanger('Network error - please try again'));
       }
     );
   };
 
   onDisableGateway = (name) => {
-    const { dispatch } = this.props;
-    const query = {
-      api: "settings",
-      params: [
-	{ category: "payment_gateways" },
-	{ action: "unset" },
-	{ data: JSON.stringify({name}) }
-      ]
-    };
-
-    apiBillRun(query).then(
-      success => {
-	dispatch(showSuccess("Payment gateway disabled!"));
-	dispatch(removePaymentGateway(name));
+    apiBillRun(disablePaymentGatewayQuery(name)).then(
+      (success) => { // eslint-disable-line no-unused-vars
+        this.props.dispatch(showSuccess('Payment gateway disabled!'));
+        this.props.dispatch(removePaymentGateway(name));
       },
-      failure => {
-	console.log('failed!', failure);
-	dispatch(showDanger("Error saving payment gateway"));
+      (failure) => {
+        console.log('failed!', failure);
+        this.props.dispatch(showDanger('Error saving payment gateway'));
       }
     ).catch(
-      error => {
-	console.log(error);
-	dispatch(showDanger("Network error - please try again"));
+      (error) => {
+        console.log(error);
+        this.props.dispatch(showDanger('Network error - please try again'));
       }
     );
   };
 
-  render() {
-    const { supported_gateways, settings } = this.props;
-    const payment_gateways = settings.get('payment_gateways', Immutable.List());
+  renderPaymentGateways = () => {
+    const { supported_gateways, payment_gateways } = this.props;
+    return supported_gateways.map((gateway, key) => {
+      const enabled = payment_gateways.find(pg => pg.get('name') === gateway.get('name'));
+      return (
+        <div className="col-lg-4 col-md-4" key={key}>
+          <PaymentGateway
+            enabled={enabled}
+            settings={gateway}
+            onDisable={this.onDisableGateway}
+            onSaveParams={this.onSaveGatewayParams}
+          />
+        </div>
+      );
+    });
+  }
 
+  render() {
     return (
       <div className="panel panel-default">
-	<div className="panel-heading">
-	  Available payment gateways
-	</div>
-	<div className="panel-body">
-	  <form className="form-horizontal">
-	    { supported_gateways.map((gateway, key) => {
-		const enabled = payment_gateways.find(pg => pg.get('name') === gateway.get('name'));
-		return (
-		  <div className="col-lg-4 col-md-4" key={key}>
-		    <PaymentGateway enabled={enabled}
-				    settings={gateway}
-				    onDisable={this.onDisableGateway}
-				    onSaveParams={this.onSaveGatewayParams} />
-		  </div>
-		);
-	      }) }
-	  </form>
-	</div>
+        <div className="panel-heading">
+          Available payment gateways
+        </div>
+        <div className="panel-body">
+          <form className="form-horizontal">
+            { this.renderPaymentGateways() }
+          </form>
+        </div>
       </div>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    settings: state.settings,
-    supported_gateways: state.list.get('supported_gateways', Immutable.List())
-  };
-}
+
+const mapStateToProps = state => ({
+  payment_gateways: state.settings.get('payment_gateways') || undefined,
+  supported_gateways: state.list.get('supported_gateways') || undefined,
+});
 
 export default connect(mapStateToProps)(PaymentGateways);
