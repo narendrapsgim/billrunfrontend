a742a6278b645e63335ab0da748c37f1a0081aed
Roman <roman@billrun.net>
2017-01-08 15:09:24 +0200

    BRCD-535: switch between UF and BF + minor refactor to avoid null in array

--- public/js/components/UsageList/Usage.js
+++ public/js/components/UsageList/Usage.js
@@ -13,19 +13,22 @@ const Usage = ({ line, onClickCancel, hiddenFields, cancelLabel }) => {
     </div>
   );
 
-  const renderFields = data => data.keySeq().map((field, key) => {
-    if (hiddenFields.includes(field) || field === 'uf') {
-      return (null);
-    }
-    return (
-      <FormGroup key={key}>
-        <Col componentClass={ControlLabel} sm={3} lg={2}>{ getFieldName(field, 'lines') }</Col>
-        <Col sm={8} lg={9}>
-          <input disabled className="form-control" value={line.get(field)} />
-        </Col>
-      </FormGroup>
-    );
-  });
+  const renderFields = (data) => {
+    const fields = [];
+    data.forEach((value, key) => {
+      if (!hiddenFields.includes(key)) {
+        fields.push(
+          <FormGroup key={key}>
+            <Col componentClass={ControlLabel} sm={3} lg={2}>{ getFieldName(key, 'lines') }</Col>
+            <Col sm={8} lg={9}>
+              <input disabled className="form-control" value={(value === null) ? '' : value} />
+            </Col>
+          </FormGroup>
+        );
+      }
+    });
+    return fields;
+  };
 
   return (
     <Row>
@@ -33,10 +36,10 @@ const Usage = ({ line, onClickCancel, hiddenFields, cancelLabel }) => {
         <Form horizontal>
           <Panel header={renderMainPanelTitle()}>
             <Panel header={<h3>BillRun fields</h3>}>
-              { renderFields(line.get('uf', Immutable.Map())) }
+              { renderFields(line) }
             </Panel>
             <Panel header={<h3>User fields</h3>}>
-              { renderFields(line) }
+              { renderFields(line.get('uf', Immutable.Map())) }
             </Panel>
             <Button onClick={onClickCancel}>{cancelLabel}</Button>
           </Panel>
@@ -48,7 +51,7 @@ const Usage = ({ line, onClickCancel, hiddenFields, cancelLabel }) => {
 
 Usage.defaultProps = {
   line: Immutable.Map(),
-  hiddenFields: ['_id', 'in_plan', 'over_plan', 'interconnect_aprice', 'out_plan'],
+  hiddenFields: ['_id', 'in_plan', 'over_plan', 'interconnect_aprice', 'out_plan', 'uf'],
   onClickCancel: () => {},
   cancelLabel: 'Back',
 };
