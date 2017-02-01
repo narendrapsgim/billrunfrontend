c2f446e3aff90e6dfcf17fb5d11fd041d837b92a
Roman <roman@billrun.net>
2017-02-01 16:18:32 +0200

    BRCD-562 - fix sortable component rerendering + improvments

--- public/js/components/CustomFields/CustomField.js
+++ public/js/components/CustomFields/CustomField.js
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Immutable from 'immutable';
-import { Row, Col, FormGroup, ControlLabel, Button } from 'react-bootstrap';
-import SettingsModal from './SettingsModal';
-import Field from '../Field';
+import changeCase from 'change-case';
+import { Form, InputGroup, Row, Col, FormGroup, ControlLabel, Button, HelpBlock } from 'react-bootstrap';
+import { SortableElement } from 'react-sortable-hoc';
+import ModalWrapper from '../Elements/ModalWrapper';
 import DragHandle from '../Elements/DragHandle';
+import Field from '../Field';
 
 
 class CustomField extends Component {
@@ -12,64 +14,125 @@ class CustomField extends Component {
   static propTypes = {
     field: PropTypes.instanceOf(Immutable.Map),
     entity: PropTypes.string.isRequired,
-    index: PropTypes.number.isRequired,
-    disabled: PropTypes.bool,
+    idx: PropTypes.number.isRequired,
+    editable: PropTypes.bool,
     onChange: PropTypes.func.isRequired,
     onRemove: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
     field: Immutable.Map(),
-    disabled: false,
+    editable: true,
   };
 
   state = {
-    showSettings: false,
+    showAdvancedEdit: false,
   }
 
   onChange = (e) => {
-    const { entity, index } = this.props;
+    const { entity, idx } = this.props;
     const { id, value } = e.target;
-    this.props.onChange(entity, index, id, value);
+    this.props.onChange(entity, idx, id, value);
   };
 
   onRemove = () => {
-    const { entity, index } = this.props;
-    this.props.onRemove(entity, index);
+    const { entity, idx } = this.props;
+    this.props.onRemove(entity, idx);
   };
 
   onCloseModal = () => {
-    this.setState({ showSettings: false });
+    this.setState({ showAdvancedEdit: false });
   };
 
+  renderAdvancedEdit = () => {
+    const { field } = this.props;
+    const { showAdvancedEdit } = this.state;
+    const modalTitle = changeCase.titleCase(`Edit ${field.get('field_name', 'filed')} Details`);
+    const checkboxStyle = { marginTop: 10, paddingLeft: 26 };
+    return (
+      <ModalWrapper show={showAdvancedEdit} onOk={this.onCloseModal} title={modalTitle}>
+        <Form horizontal>
+          <FormGroup>
+            <Col sm={3} componentClass={ControlLabel}>Unique</Col>
+            <Col sm={9} style={checkboxStyle}>
+              <Field id="unique" onChange={this.onChange} value={field.get('unique', false)} fieldType="checkbox" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col sm={3} componentClass={ControlLabel}>Mandatory</Col>
+            <Col sm={9} style={checkboxStyle}>
+              <Field id="mandatory" onChange={this.onChange} value={field.get('mandatory', false)} fieldType="checkbox" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col sm={3} componentClass={ControlLabel}>Editable</Col>
+            <Col sm={9} style={checkboxStyle}>
+              <Field id="editable" onChange={this.onChange} value={field.get('editable', false)} fieldType="checkbox" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col sm={3} componentClass={ControlLabel}>Display</Col>
+            <Col sm={9} style={checkboxStyle}>
+              <Field id="display" onChange={this.onChange} value={field.get('display', false)} fieldType="checkbox" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col sm={3} componentClass={ControlLabel}>Show in list</Col>
+            <Col sm={9} style={checkboxStyle}>
+              <Field id="show_in_list" onChange={this.onChange} value={field.get('show_in_list', false)} fieldType="checkbox" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col sm={3} componentClass={ControlLabel}>Select list</Col>
+            <Col sm={9}>
+              <InputGroup>
+                <InputGroup.Addon>
+                  <Field id="select_list" onChange={this.onChange} value={field.get('select_list', false)} fieldType="checkbox" />
+                </InputGroup.Addon>
+                <Field id="select_options" onChange={this.onChange} value={field.get('select_options', '')} disabled={!field.get('select_list', false)} />
+              </InputGroup>
+              { field.get('select_list', false) && <HelpBlock style={{ marginLeft: 40 }}>Select Options <small>(comma-separated list)</small></HelpBlock>}
+            </Col>
+          </FormGroup>
+
+        </Form>
+      </ModalWrapper>
+    );
+  }
+
   renderField = () => {
-    const { field, disabled } = this.props;
+    const { field, editable } = this.props;
     return (
       <Row>
         <Col lg={1} md={1} style={{ paddingTop: 10 }}><DragHandle /></Col>
         <Col lg={2} md={2}>
           <FormGroup>
             <ControlLabel>Field Name</ControlLabel>
-            <Field id="field_name" onChange={this.onChange} value={field.get('field_name', '')} disabled={disabled} />
+            <Field id="field_name" onChange={this.onChange} value={field.get('field_name', '')} disabled={!editable} />
           </FormGroup>
         </Col>
         <Col lg={2} md={2}>
           <FormGroup>
             <ControlLabel>Title</ControlLabel>
-            <Field id="title" onChange={this.onChange} value={field.get('title', '')} disabled={disabled} />
+            <Field id="title" onChange={this.onChange} value={field.get('title', '')} disabled={!editable} />
           </FormGroup>
         </Col>
         <Col lg={2} md={2}>
           <FormGroup>
             <ControlLabel>Default Value</ControlLabel>
-            <Field id="default_value" onChange={this.onChange} value={field.get('default_value', '')} disabled={disabled} />
+            <Field id="default_value" onChange={this.onChange} value={field.get('default_value', '')} disabled={!editable} />
           </FormGroup>
         </Col>
         <Col lg={3} md={3}>
           <FormGroup>
             <ControlLabel>&nbsp;</ControlLabel>
             <div className="text-center">
-              {!disabled && <button className="btn btn-link" onClick={() => this.setState({ showSettings: true })}> Advanced </button> }
+              {editable && <button className="btn btn-link" onClick={() => this.setState({ showAdvancedEdit: true })}> Advanced </button> }
             </div>
           </FormGroup>
         </Col>
@@ -77,7 +140,7 @@ class CustomField extends Component {
           <FormGroup>
             <ControlLabel>&nbsp;</ControlLabel>
             <div>
-              {!disabled && <Button onClick={this.onRemove} bsSize="small"><i className="fa fa-trash-o danger-red" /> Remove </Button> }
+              {editable && <Button onClick={this.onRemove} bsSize="small"><i className="fa fa-trash-o danger-red" /> Remove </Button> }
             </div>
           </FormGroup>
         </Col>
@@ -86,21 +149,13 @@ class CustomField extends Component {
   }
 
   render() {
-    const { field } = this.props;
-    const { showSettings } = this.state;
-
     return (
-      <div className="CustomField">
-        {this.renderField()}
-        <SettingsModal
-          field={field}
-          onClose={this.onCloseModal}
-          onChange={this.onChange}
-          show={showSettings}
-        />
+      <div className="CustomField" style={{ borderTop: '1px solid #eee', borderBottom: '1px solid #eee', paddingTop: 10 }}>
+        { this.renderField() }
+        { this.renderAdvancedEdit() }
       </div>
     );
   }
 }
 
-export default connect()(CustomField);
+export default connect()(SortableElement(CustomField));
--- public/js/components/CustomFields/CustomFields.js
+++ public/js/components/CustomFields/CustomFields.js
@@ -3,8 +3,10 @@ import { connect } from 'react-redux';
 import Immutable from 'immutable';
 import { Tabs, Tab, Panel } from 'react-bootstrap';
 import { ucFirst } from 'change-case';
-import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import CustomField from './CustomField';
+import ActionButtons from '../Elements/ActionButtons';
+import CreateButton from '../Elements/CreateButton';
+import SortableFieldsContainer from './SortableFieldsContainer';
 import { getSettings, updateSetting, removeSettingField, saveSettings, setFieldPosition } from '../../actions/settingsActions';
 
 class CustomFields extends Component {
@@ -48,11 +50,18 @@ class CustomFields extends Component {
     this.props.dispatch(removeSettingField('subscribers', [entity, 'fields', index]));
   };
 
-  onAddNewField = entity => () => {
+  onAddNewField = () => {
+    const { tab } = this.state;
+    const { tabs } = this.props;
+    const entity = tabs[tab];
     const size = this.props[entity].size;
     const newField = Immutable.Map();
     this.props.dispatch(updateSetting('subscribers', [entity, 'fields', size], newField));
   };
+
+  onClickCancel = () => {
+    this.props.dispatch(getSettings('subscribers'));
+  }
 
   onClickSave = () => {
     this.props.dispatch(saveSettings('subscribers'));
@@ -77,43 +86,33 @@ class CustomFields extends Component {
     entityFields.forEach((field, index) => {
       if (!field.get('generated', false) && !defaultHiddenFields.includes(field.get('field_name', ''))) {
         const editable = !field.get('system', false) && !defaultDisabledFields.includes(field.get('field_name', ''));
-        const SortableItem = SortableElement(props => (
-          <div style={{ borderTop: '1px solid #eee', borderBottom: '1px solid #eee', paddingTop: 10 }}>
-            <CustomField
-              field={props.field}
-              entity={props.entity}
-              index={props.idx}
-              disabled={!props.editable}
-              onChange={this.onChangeField}
-              onRemove={this.onRemoveField}
-            />
-          </div>
-        ));
         fields.push(
-          <SortableItem
+          <CustomField
             key={`item-${index}`}
             index={index}
+            idx={index}
             field={field}
             entity={entity}
             editable={editable}
-            idx={index}
+            onChange={this.onChangeField}
+            onRemove={this.onRemoveField}
           />
         );
       }
     });
-    const SortableList = SortableContainer(({ sortableFields }) =>
-      (<Panel style={{ borderTop: 'none' }}>{sortableFields}</Panel>)
-    );
+
     return (
       <Tab key={key} title={`${ucFirst(entity)} Fields`} eventKey={key}>
-        <SortableList
-          lockAxis="y"
-          helperClass="draggable-menu"
-          useDragHandle={true}
-          sortableFields={fields}
-          onSortEnd={this.onSortEnd}
-        />
-        <button type="button" className="btn btn-link" onClick={this.onAddNewField(entity)}> Add New Field </button>
+        <Panel style={{ borderTop: 'none' }}>
+          <SortableFieldsContainer
+            lockAxis="y"
+            helperClass="draggable-menu"
+            useDragHandle={true}
+            items={fields}
+            onSortEnd={this.onSortEnd}
+          />
+          <CreateButton onClick={this.onAddNewField} type="Field" style={{ marginTop: 15 }} />
+        </Panel>
       </Tab>
     );
   };
@@ -125,10 +124,7 @@ class CustomFields extends Component {
         <Tabs id="CustomFieldsTabs" animation={false} onSelect={this.onSelectTab}>
           { tabs.map(this.renderFieldsTab) }
         </Tabs>
-        <div style={{ marginTop: 12 }}>
-          <button type="submit" className="btn btn-primary" onClick={this.onClickSave} style={{ marginRight: 10 }} > Save </button>
-          <button type="reset" className="btn btn-default" onClick={this.onClickCancel} > Cancel </button>
-        </div>
+        <ActionButtons onClickSave={this.onClickSave} onClickCancel={this.onClickCancel} />
       </div>
     );
   }
