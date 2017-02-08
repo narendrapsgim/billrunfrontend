244201e131feb7ff0189f48c29e242d8568deb37
Roman <roman@billrun.net>
2017-02-08 13:48:36 +0200

    InvoicesList - format dates

--- public/js/components/InvoicesList/InvoicesList.js
+++ public/js/components/InvoicesList/InvoicesList.js
@@ -104,8 +104,8 @@ class InvoicesList extends Component {
 
   getTableFields = () => ([
     { id: 'invoice_id', title: 'Invoice Id', sort: true },
-    { id: 'invoice_date', title: 'Date', cssClass: 'short-date', sort: true },
-    { id: 'due_date', title: 'Due', cssClass: 'short-date', sort: true },
+    { id: 'invoice_date', title: 'Date', cssClass: 'short-date', sort: true, type: 'date' },
+    { id: 'due_date', title: 'Due', cssClass: 'short-date', sort: true, type: 'date' },
     { id: 'amount', title: 'Amount', sort: true },
     { id: 'paid_by', title: 'Status', parser: this.parserPaidBy },
     { id: 'billrun_key', title: 'Cycle', sort: true },
