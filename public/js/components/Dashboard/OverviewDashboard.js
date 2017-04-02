5f6776035affb286972575b6681e7d9e93747019
Roman <roman@billrun.net>
2017-04-02 15:41:41 +0300

    Demo data OverviewDashboard

--- public/js/components/Dashboard/OverviewDashboard.js
+++ public/js/components/Dashboard/OverviewDashboard.js
@@ -2,12 +2,33 @@ import React, { PropTypes } from 'react';
 import { Row, Panel, Col } from 'react-bootstrap';
 import moment from 'moment';
 import DashboardBase from './DashboardBase';
+import DoughnutSelectable from './Widgets/DoughnutSelectable'
 
+
+const dataA = {
+  labels: ['Existing', 'New', 'Dorm.', 'Churn'],
+  values: [84000, 21000, 7000, 3000],
+};
+
+const datab = {
+  labels: ['Plan A', 'Plan B', 'Plan C', 'Plan D'],
+  values: [200, 184, 57, 99],
+  sign: [1, 0, -1, 6],
+};
 
 const OverviewDashboard = ({ fromDate, toDate }) => (
   <Row>
-    <Col lg={6}>
-      <p>OverviewDashboard</p>
+    <Col sm={12}>
+      <Col sm={6}>
+        <Panel header="Customer Sate Distribution">
+          <DoughnutSelectable data={dataA} />
+        </Panel>
+      </Col>
+      <Col sm={6}>
+        <Panel header="Revenue by Payment type">
+          <DoughnutSelectable data={datab} type="details" />
+        </Panel>
+      </Col>
     </Col>
   </Row>
 );
