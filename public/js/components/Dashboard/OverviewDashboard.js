a02eaeccb1b39ec750f376df03c0b57085f7032c
Roman <roman@billrun.net>
2017-04-03 16:05:37 +0300

    BRCD-693 : OverviewDashboard with demo data

--- public/js/components/Dashboard/OverviewDashboard.js
+++ public/js/components/Dashboard/OverviewDashboard.js
@@ -2,40 +2,45 @@ import React, { PropTypes } from 'react';
 import { Row, Panel, Col } from 'react-bootstrap';
 import moment from 'moment';
 import DashboardBase from './DashboardBase';
-import DoughnutSelectable from './Widgets/DoughnutSelectable'
+import DoughnutSelectable from './Widgets/DoughnutSelectable';
+import PercentBar from './Widgets/PercentBar';
 
+import {
+  fakeDoughnutDetails,
+  fakeDoughnutLegend,
+  fakePercentBar,
+} from './fakeData';
 
-const dataA = {
-  labels: ['Existing', 'New', 'Dorm.', 'Churn'],
-  values: [84000, 21000, 7000, 3000],
-};
-
-const datab = {
-  labels: ['Plan A', 'Plan B', 'Plan C', 'Plan D'],
-  values: [200, 184, 57, 99],
-  sign: [1, 0, -1, 6],
-};
-
-const OverviewDashboard = ({ fromDate, toDate }) => (
-  <Row>
-    <Col sm={12}>
-      <Col sm={6}>
-        <Panel header="Customer Sate Distribution">
-          <DoughnutSelectable data={dataA} />
-        </Panel>
+const OverviewDashboard = ({ fromDate, toDate, currency }) => {
+  const parseCurrencyValue = value => Number(value).toLocaleString('en-US', { style: 'currency', currency });
+  const parseCountValue = value => Number(value).toLocaleString('en-US');
+  return (
+    <Row>
+      <Col sm={12}>
+        <Col sm={6}>
+          <Panel header={`Customer Sate Distribution ${currency}`}>
+            <DoughnutSelectable data={fakeDoughnutLegend()} parseValue={parseCountValue} />
+          </Panel>
+        </Col>
+        <Col sm={6}>
+          <Panel header="Revenue by Payment type">
+            <DoughnutSelectable data={fakeDoughnutDetails()} type="details" parseValue={parseCurrencyValue} />
+          </Panel>
+        </Col>
+        <Col sm={6}>
+          <Panel header="Revenue by Payment type">
+            <PercentBar data={fakePercentBar()} parseValue={parseCurrencyValue} />
+          </Panel>
+        </Col>
       </Col>
-      <Col sm={6}>
-        <Panel header="Revenue by Payment type">
-          <DoughnutSelectable data={datab} type="details" />
-        </Panel>
-      </Col>
-    </Col>
-  </Row>
-);
+    </Row>
+  );
+};
 
 OverviewDashboard.propTypes = {
   fromDate: PropTypes.instanceOf(moment),
   toDate: PropTypes.instanceOf(moment),
+  currency: PropTypes.string,
 };
 
 export default DashboardBase(OverviewDashboard);
