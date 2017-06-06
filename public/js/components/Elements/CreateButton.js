a8baf9ed4508a9351efce46478d5d813a91de966
Roman <roman@billrun.net>
2017-06-06 21:26:21 +0300

    Elements - CreateButton: added new param Disable

--- public/js/components/Elements/CreateButton.js
+++ public/js/components/Elements/CreateButton.js
@@ -1,8 +1,8 @@
 import React, { PropTypes } from 'react';
 import { Button } from 'react-bootstrap';
 
-const CreateButton = ({ label, onClick, type }) => (
-  <Button bsSize="xsmall" className="btn-primary" onClick={onClick} style={{ marginTop: 15 }}>
+const CreateButton = ({ label, onClick, type, disabled }) => (
+  <Button bsSize="xsmall" className="btn-primary" onClick={onClick} style={{ marginTop: 15 }} disabled={disabled}>
     <i className="fa fa-plus" />&nbsp;{label}{type.length > 0 && ` ${type}`}
   </Button>
 );
@@ -11,12 +11,14 @@ const CreateButton = ({ label, onClick, type }) => (
 CreateButton.defaultProps = {
   label: 'Create New',
   type: '',
+  disabled: false,
   onClick: () => {},
 };
 
 CreateButton.propTypes = {
   label: PropTypes.string,
   type: PropTypes.string,
+  disabled: PropTypes.bool,
   onClick: PropTypes.func,
 };
 
