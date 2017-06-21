216b6af11d7e7b1a54bcd705cdae45d929f112ac
Roman <roman@billrun.net>
2017-06-21 12:26:54 +0300

    BRCD-692: onBoarding - POC dragable/minimizebla + invoice HTML

--- public/js/components/OnBoarding/OnBoarding.js
+++ public/js/components/OnBoarding/OnBoarding.js
@@ -1,7 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { ModalWrapper } from '../Elements';
+import classNames from 'classnames';
+import { Button, Panel, ButtonGroup } from 'react-bootstrap';
+import renderHTML from 'react-render-html';
+import Draggable from 'react-draggable';
+import { ModalWrapper, DragHandle } from '../Elements';
 import { showOnBoarding } from '../../actions/guiStateActions/pageActions';
+import htmlInvoce from './invoice.html';
 
 
 class OnBoarding extends Component {
@@ -16,18 +21,61 @@ class OnBoarding extends Component {
     show: false,
   };
 
+  state = {
+    state: 'maximize',
+  };
+
 
   onCloseImprt = () => {
     const { show } = this.props;
     this.props.dispatch(showOnBoarding(!show));
   }
 
+  onMinimize = () => {
+    this.setState({ state: 'minimize' });
+  }
+
+  onMaximize = () => {
+    this.setState({ state: 'maximize' });
+  }
+
+  renderHeader = () => {
+    const { state } = this.state;
+    return (
+      <div>
+        <div className="handle pull-left"><DragHandle /></div>
+        &nbsp;
+        <ButtonGroup className="pull-right">
+          { (state === 'maximize')
+            ? <Button onClick={this.onMinimize} bsSize="small"><i className="fa fa-window-minimize" /></Button>
+            : <Button onClick={this.onMaximize} bsSize="small"><i className="fa fa-window-maximize" /></Button>
+          }
+          <Button onClick={this.onCloseImprt} bsSize="small"><i className="fa fa-window-close" /></Button>
+        </ButtonGroup>
+      </div>
+    );
+  }
+
   render() {
     const { show } = this.props;
+    const { state } = this.state;
+    if (!show) {
+      return null;
+    }
+
+    const wrapperClass = classNames('on-boarding-wrapper', {
+      minimized: state === 'minimize',
+    });
     return (
-      <ModalWrapper show={show} title="Help" onHide={this.onCloseImprt}>
-        <h1>On Boarding ...</h1>
-      </ModalWrapper>
+      <Draggable handle=".handle" defaultPosition={{ x: 200, y: 50 }}>
+        <Panel className={wrapperClass} header={this.renderHeader()}>
+          <h1>We will take you to a tour...</h1>
+          <div className="text-center">
+            <Button>Start invoice tour</Button>
+          </div>
+          {renderHTML(htmlInvoce)}
+        </Panel>
+      </Draggable>
     );
   }
 }
