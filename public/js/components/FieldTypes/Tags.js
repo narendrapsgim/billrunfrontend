dcead374963e3c490ee184a21e56fa9b14be3760
Roman <roman@billrun.net>
2017-06-25 13:23:26 +0300

    Tags Input: new prop function to override default getDisplayValue for Tag value.

--- public/js/components/FieldTypes/Tags.js
+++ public/js/components/FieldTypes/Tags.js
@@ -5,12 +5,13 @@ const Tags = (props) => {
   const { editable, value, disabled, placeholder, onChange, inputProps, ...otherProps } = props;
 
   const renderTag = (args) => {
-    const { tag, key, disabled: allowRemove, onRemove, classNameRemove, getTagDisplayValue, ...other } = args;
+    const { tag, key, disabled: allowRemove, onRemove, classNameRemove, ...other } = args;
     const remove = () => { onRemove(key); };
+    const renderDisplayValue = props.getTagDisplayValue || args.getTagDisplayValue;
     return (
       <span key={key} {...other}>
         {!allowRemove && (<span className={classNameRemove} onClick={remove}>×</span>)}
-        {getTagDisplayValue(tag)}
+        {renderDisplayValue(tag)}
       </span>
     );
   };
@@ -40,6 +41,7 @@ Tags.defaultProps = {
   placeholder: '',
   inputProps: {},
   onChange: () => {},
+  getTagDisplayValue: null,
 };
 
 Tags.propTypes = {
@@ -49,6 +51,7 @@ Tags.propTypes = {
   placeholder: PropTypes.string,
   inputProps: PropTypes.object,
   onChange: PropTypes.func,
+  getTagDisplayValue: PropTypes.func,
 };
 
 export default Tags;
