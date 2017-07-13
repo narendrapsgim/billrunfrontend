c467377c25ade26d92e76337613f176ab199574c
Roman <roman@billrun.net>
2017-07-13 20:58:39 +0300

    BRCD-692: OnBoarding - added subscription steps

--- public/js/components/OnBoarding/Invoice.js
+++ public/js/components/OnBoarding/Invoice.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import htmlHeader from './assests/header.html';
-import htmlSummary from './assests/summary.html';
+import Header from './assests/Header';
+import htmlHeaderCustomer from './assests/header-customer.html';
+import htmlHeaderSubscriber from './assests/header-subscriber.html';
+import htmlSummaryCustomer from './assests/summary-customer.html';
+import htmlSummarySubscription from './assests/summary-subscription.html';
+import htmlLineItems from './assests/line-items.html';
 import htmlFooter from './assests/footer.html';
 import './assests/style/invoice.scss';
 import './assests/style/font.scss';
 
 const Invoice = () => (
   <div className="invoice-help">
-    <div dangerouslySetInnerHTML={{ __html: htmlHeader }} />
-    <div dangerouslySetInnerHTML={{ __html: htmlSummary }} />
+    <Header page="1" total="2" />
+    <div dangerouslySetInnerHTML={{ __html: htmlHeaderCustomer }} />
+    <div dangerouslySetInnerHTML={{ __html: htmlSummaryCustomer }} />
+    <div dangerouslySetInnerHTML={{ __html: htmlFooter }} />
+    <hr className="page-separator" />
+    <Header page="2" total="2" />
+    <div dangerouslySetInnerHTML={{ __html: htmlHeaderSubscriber }} />
+    <div dangerouslySetInnerHTML={{ __html: htmlSummarySubscription }} />
+    <div dangerouslySetInnerHTML={{ __html: htmlLineItems }} />
     <div dangerouslySetInnerHTML={{ __html: htmlFooter }} />
   </div>
 );
