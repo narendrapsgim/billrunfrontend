fa90848a8f610890e8a820e6efb33a32783c7a1d
Roman <roman@billrun.net>
2017-07-23 19:04:07 +0300

    BRCD-910: remove entity status 'active_with_future' add new flag 'is_last'

--- public/js/common/Util.js
+++ public/js/common/Util.js
@@ -142,7 +142,7 @@ export const isItemReopened = (item, prevItem) => {
   return currentFrom.isAfter(prevTo.add(1, 'days'));
 };
 
-export const isItemExpired = (item, toField = 'to') => {
+export const isItemFinite = (item, toField = 'to') => {
   const toTime = getItemDateValue(item, toField);
   return toTime.isBefore(moment().add(50, 'years'));
 };
@@ -153,7 +153,8 @@ export const getItemMode = (item, undefindItemStatus = 'create') => {
       return 'create';
     }
     const status = item.getIn(['revision_info', 'status'], '');
-    if (['expired', 'active_with_future'].includes(status)) {
+    const isLast = item.getIn(['revision_info', 'is_last'], true);
+    if (['expired'].includes(status) || (status === 'active' && !isLast)) {
       return 'view';
     }
     if (['future'].includes(status) || isItemClosed(item)) {
