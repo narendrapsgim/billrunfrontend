99aeb84bea4e7e1a0a2cbcf37fa490066ad3f6cf
Roman <roman@billrun.net>
2017-08-01 18:44:28 +0300

    BRCD-901: fix(Report): auto focus input type number in tags input

--- public/js/components/Report/Editor/ConditionValue.js
+++ public/js/components/Report/Editor/ConditionValue.js
@@ -159,14 +159,7 @@ class ConditionValue extends Component {
   }
 
   renderCustomInputNumber =({ addTag, onChange, value, ...other }) => (
-    <span className="custom-field-input">
-      <Field
-        fieldType="number"
-        onChange={onChange}
-        value={value}
-        {...other}
-      />
-    </span>
+    <input type="number" onChange={onChange} value={value} {...other} />
   );
 
   render() {
