c9288d6086600c55b01a6708503c7cc48dd5f5c9
Roman <roman@billrun.net>
2018-12-04 17:37:41 +0200

    BRCD-1642 fix(EventFraud) - fix the Threshold value<->op onchnage callback

--- public/js/components/Events/Elements/FraudEventThreshold.js
+++ public/js/components/Events/Elements/FraudEventThreshold.js
@@ -28,7 +28,7 @@ const FraudEventThreshold = (props) => {
   };
   const onChangeThresholdValue = (e) => {
     const { value } = e.target;
-    onUpdate(['threshold_conditions', 0, index, 'op'], value);
+    onUpdate(['threshold_conditions', 0, index, 'value'], value);
   };
   const onChangeThresholdUnit = (value) => {
     onUpdate(['threshold_conditions', 0, index, 'unit'], value);
