88646e145b953a66681f290646583f5bdb96e7b0
Snir Cohen <snir.cohen@billrun.com>
2019-03-03 11:05:06 +0200

    BRCD-1489 - Update product UoM - UI. Fixed text overlapping due to css.

--- public/js/components/Product/Product.js
+++ public/js/components/Product/Product.js
@@ -301,7 +301,7 @@ class Product extends Component {
                     )
                     : (
                       <div>
-                        <Col sm={1} style={{ paddingTop: 7 }}>{usaget}</Col>
+                        <Col sm={2} style={{ paddingTop: 7 }}>{usaget}</Col>
                         <Col sm={2} componentClass={ControlLabel}>Units of Measure</Col>
                         <Col sm={2}>
                           <UsageTypesSelector
