b6c1cf56687d6978c610a69af1a3b8f5329e9839
Roman <roman@billrun.net>
2019-04-28 09:56:51 +0300

    refactor(CreateButton) - use useCallback hook to optimize

--- src/components/Elements/CreateButton.js
+++ src/components/Elements/CreateButton.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
-const CreateButton = ({ label, onClick, type, action, disabled, title, data, buttonStyle }) =>
-  <Button bsSize="xsmall" className="btn-primary" onClick={() => onClick(data)} style={buttonStyle} disabled={disabled} title={title}>
-    <i className="fa fa-plus" />
-    {action.length > 0 && ` ${action}`}
-    {label.length > 0 && ` ${label}`}
-    {type.length > 0 && ` ${type}`}
-  </Button>
-
+const CreateButton = ({
+  label, onClick, type, action, disabled, title, data, buttonStyle,
+}) => {
+  const cachedOnClick = useCallback(() => onClick(data), [onClick, data]);
+  return (
+    <Button
+      bsSize="xsmall"
+      className="btn-primary"
+      onClick={cachedOnClick}
+      style={buttonStyle}
+      disabled={disabled}
+      title={title}
+    >
+      <i className="fa fa-plus" />
+      {action.length > 0 && ` ${action}`}
+      {label.length > 0 && ` ${label}`}
+      {type.length > 0 && ` ${type}`}
+    </Button>
+  );
+}
 
 CreateButton.defaultProps = {
   label: 'New',
