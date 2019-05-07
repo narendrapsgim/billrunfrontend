22d55151e9494ffb792e1f05585070af76c6b750
Roman <roman@billrun.net>
2019-05-07 18:29:22 +0300

    feta(Actions) - new format as buttonGroup

--- src/components/Elements/Actions/Actions.js
+++ src/components/Elements/Actions/Actions.js
@@ -2,37 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Action from './Action';
+import { ButtonGroup } from 'react-bootstrap';
 
-const Actions = ({ actions, data }) => (
-  <div className="actions-buttons">
-    { actions.map((action, idx, list) => {
-      const isLast = idx === (list.length - 1);
-      let isEnable = true;
-      if (typeof action.show !== 'undefined') {
-        isEnable = (typeof action.show === 'function') ? action.show(data) : action.show;
-      }
-      const actionClass = classNames({
-        mr10: !isLast && isEnable,
-        mr0: isLast || !isEnable,
-      });
-      return (
-        <span key={idx} className={actionClass} >
-          <Action {...action} data={data} />
-        </span>
-      );
-    })}
-  </div>
-);
-
+const Actions = ({ actions, data, isGroup }) => {
+  if (isGroup) {
+    return (
+      <ButtonGroup className="actions-buttons">
+        { actions.map((action, idx) => (<Action {...action} data={data} key={idx} />))}
+      </ButtonGroup>
+    );
+  }
+  return (
+    <div className="actions-buttons">
+      { actions.map((action, idx, list) => {
+        const isLast = idx === (list.length - 1);
+        let isEnable = true;
+        if (typeof action.show !== 'undefined') {
+          isEnable = (typeof action.show === 'function') ? action.show(data) : action.show;
+        }
+        const actionClass = classNames({
+          mr10: !isLast && isEnable,
+          mr0: isLast || !isEnable,
+        });
+        return (
+          <span key={idx} className={actionClass} >
+            <Action {...action} data={data} />
+          </span>
+        );
+      })}
+    </div>
+  );
+}
 
 Actions.defaultProps = {
   actions: [],
   data: null,
+  isGroup: false,
 };
 
 Actions.propTypes = {
   actions: PropTypes.arrayOf(PropTypes.object),
   data: PropTypes.any,
+  isGroup: PropTypes.bool,
 };
 
 export default Actions;
