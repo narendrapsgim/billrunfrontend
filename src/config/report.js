e6ff33e61f6772dffa16b2edf7a86f525cfca2eb
killawatt <shani@billrun.net>
2019-07-07 15:48:35 +0300

    Fix bills' due field's title in reports

--- src/config/report.js
+++ src/config/report.js
@@ -158,7 +158,7 @@ export default {
       { id: 'aid', type: 'number', title: 'Customer ID' },
       { id: 'invoice_id', type: 'number', title: 'Invoice ID' },
       { id: 'due_date', type: 'date', title: 'Due Date' },
-      { id: 'due', type: 'number', title: 'Original Due Date' },
+      { id: 'due', type: 'number', title: 'Original Due Amount' },
       { id: 'payer_name', type: 'string', title: 'Payer Name' },
       { id: 'amount', type: 'number', title: 'Original Absolute Due Amount' },
       { id: 'lastname', type: 'string', title: 'Customer\'s Last Name' },
