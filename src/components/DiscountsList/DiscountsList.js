022487f531377ccb6f0dfc5d0d10d5ccfdb8cb2c
Roman <roman@billrun.net>
2019-08-05 17:48:07 +0300

    BRCD-1938 feat(Discounts) - added Export discounts action to discounts list

--- src/components/DiscountsList/DiscountsList.js
+++ src/components/DiscountsList/DiscountsList.js
@@ -1,44 +1,76 @@
-import React from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import EntityList from '../EntityList';
+import { ExporterPopup } from '@/components/Exporter';
 import { getFieldName } from '@/common/Util';
 
 
+const filterFields = [
+  { id: 'description', placeholder: 'Title' },
+  { id: 'key', placeholder: 'Key' },
+];
+
+const projectFields = {
+  key: 1,
+  description: 1,
+  type: 1,
+};
+
+const parseType = (item) => item.get('type', '') === 'percentage'
+  ? getFieldName('type_percentage', 'discount')
+  : getFieldName('type_monetary', 'discount')
+
+
+const tableFields = [
+  { id: 'description', title: 'Title', sort: true },
+  { id: 'key', title: 'Key', sort: true },
+  { id: 'type', title: 'Type', sort: true, parser: parseType },
+];
+
+const actions = [
+  { type: 'edit' },
+];
+
+
 const DiscountsList = () => {
-  const filterFields = [
-    { id: 'description', placeholder: 'Title' },
-    { id: 'key', placeholder: 'Key' },
-  ];
 
-  const parseType = (item) => {
-    return item.get('type', '') === 'percentage'
-      ? getFieldName('type_percentage', 'discount')
-      : getFieldName('type_monetary', 'discount');
-  }
+  const [visibleExport, toggleExpoer] = useState(false);
 
-  const tableFields = [
-    { id: 'description', title: 'Title', sort: true },
-    { id: 'key', title: 'Key', sort: true },
-    { id: 'type', title: 'Type', sort: true, parser: parseType },
-  ];
+  const showExport = useCallback(() => {
+      toggleExpoer(true)
+  }, [toggleExpoer]);
 
-  const projectFields = {
-    key: 1,
-    description: 1,
-    type: 1,
-  };
+  const hideExport = useCallback(() => {
+      toggleExpoer(false)
+  }, [toggleExpoer]);
 
-  const actions = [
-    { type: 'edit' },
-  ];
+  const listActions = useMemo(() => ([{
+    type: 'add',
+  }, {
+    type: 'refresh',
+  }, {
+    type: 'export_csv',
+    label: 'Export',
+    onClick: showExport,
+    actionStyle: 'primary',
+    actionSize: 'xsmall',
+  }]), [showExport]);
 
   return (
-    <EntityList
-      entityKey="discount"
-      filterFields={filterFields}
-      tableFields={tableFields}
-      projectFields={projectFields}
-      actions={actions}
-    />
+    <>
+      <EntityList
+        entityKey="discount"
+        filterFields={filterFields}
+        tableFields={tableFields}
+        projectFields={projectFields}
+        actions={actions}
+        listActions={listActions}
+      />
+      <ExporterPopup
+        entityKey="discount"
+        show={visibleExport}
+        onClose={hideExport}
+      />
+    </>
   );
 };
 
