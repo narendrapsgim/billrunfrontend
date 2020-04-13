82c58e4608ac8a19afa98d261a8efa30381ffb7e
Roman <roman.edneral@billrun.com>
2020-04-13 18:27:29 +0300

    BRCD-2491 fix(Import) - convert import mapper to prevent using '.' as json keys

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -2,6 +2,7 @@ import Immutable from 'immutable';
 import { actions } from '@/actions/settingsActions';
 import { ADD_USAGET_MAPPING } from '@/actions/inputProcessorActions';
 import { LOGOUT } from '@/actions/userActions';
+import { convertImporterMapperFromDb } from '@/actions/importerActions';
 import { getConfig } from '@/common/Util';
 
 
@@ -56,6 +57,9 @@ export default function (state = defaultState, action) {
           let data = Immutable.fromJS(setting.data.details);
           if (setting.name === 'taxation' && data.get('vat', '') !== '') {
             data = data.set('vat', data.get('vat', '') * 100);
+          }
+          if (setting.name === 'import.mapping' && Immutable.List.isList(data)) {
+            data = data.map(convertImporterMapperFromDb);
           }
           if (setting.name === 'taxation.vat' && data !== '') {
             data *= 100;
